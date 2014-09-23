f85d08414fb598a51dd4eea67b1d8168b83bc04b
Brian A. Wandell <wandell@stanford.edu>
2014-09-22 21:16:12 -0700

    ENH: Added more code for forward computations

--- tutorial/t_lifeSimulation.m
+++ tutorial/t_lifeSimulation.m
@@ -1,4 +1,18 @@
-%% A script showing how to perform a virtual lesion
+%% A script showing how to create simulation ground-truth data
+%
+% The ground-truth data are built by the script s_make_dtiinit.m
+%
+% This 
+%  FP took a data set from Stanford at 1.5mm 96-directions from the LiFE
+%  paper and resampled to 4mm. 
+%  Purpose: Smaller data size.  This is in the raw/ directory.
+%
+%  Then he run dtiInit on the data and created the init log and the
+%  dti96trilin directory.  The additional files are produced by dtiInit as
+%  well.
+% 
+%  Then he ran mrTrix to create a whole-brain connectome.  He requested
+%  160K fibers and Lmax = 10, probabilistic.
 %
 % Processing steps:
 %  * The script starts with raw diffusion data
@@ -10,91 +24,148 @@
 %
 % FP Vistasoft Lab, Stanford, 2014
 
-%% Set the path and run dtiInit
+%% Load the AFQ Fibers and merge them into a single
 
-% cube mm of the spatial resolution for the output diffusion data
-resolution = 4; 
+dataDir = fullfile(mrvDataRootPath,'life','data');
+afqDir  = fullfile(dataDir,'AFQ');
+fascicleList = dir(fullfile(afqDir,'*.mat'));
 
-% Build the new file name for the dt6 folder
-res    = num2str(resolution); 
-idx    = strfind(res,'.'); 
-if ~isempty(idx), res(2) ='p';end
+% The last one is the tracts_classification_indices, which we don't want in
+% the list.  Typically, there are 20 tracts.
+fascicleList = fascicleList(1:(end-1));
 
-% File name of the input diffusion data
-%dwi_raw_file  = 'dwi_data_b2000_1p25mm';
-dwi_raw_file  = 'run01_fliprot_aligned_trilin';
-dt6_dir_name  = sprintf('dt6_%s_%smm',dwi_raw_file(1:14),res);
-%dataDir       = '/home/frk/2t1/HCP/105115_data_variability/';
-dataDir       = fullfile(mrvDataRootPath,'life');
-dwRawFileName = fullfile(dataDir,dt6_dir_name, 'raw', sprintf('%s.nii.gz',dwi_raw_file));
-if ~exist(dwRawFileName,'file'), error('No dwRaw file %s',dwRawFileName); end
-t1FileName    = fullfile(dataDir,dt6_dir_name, 'anatomy','t1.nii.gz');
-if ~exist(t1FileName,'file'), error('No T1 File %s',t1FileNames); end
+%% Merge all the fascicles into a single, whole-brain group
 
-% Initialization parameters
-dwp = dtiInitParams;
-dwp.eddyCorrect    = false;
-dwp.phaseEncodeDir = 2;
-%dwParams.rotateBvecsWithCanXform = 1;
-  
-% Set the spatial resolution of the output diffusion data
-dwp.dwOutMm = [resolution resolution resolution];
+% When we merge, we may not take all the fascicles within a tract
+sampF = 0.08;
+for ii=1:length(fascicleList)
+    fg = fgRead(fullfile(afqDir,fascicleList(ii).name));
+    n = fgGet(fg,'n fibers');
+    idx = randsample(1:n,ceil(sampF*n));
+    fg = fgExtract(fg,idx,'keep');
+    if ii==1
+        fgAll = fg;
+    else
+        % fgMerge should be able to take a list of fullpath file names.
+        fgAll = fgMerge(fgAll,fg);
+    end 
+end
+fgAll = fgSet(fgAll,'name','AFQ Merged 4mm');
 
-dwp.dt6BaseName = '';
-dwp.bvecsFile   = fullfile(dataDir,dt6_dir_name, 'raw',sprintf('%s.bvecs',dwi_raw_file));
-dwp.bvalsFile   = fullfile(dataDir,dt6_dir_name, 'raw',sprintf('%s.bvals',dwi_raw_file));
+% Visualize now
+% mbaDisplayConnectome(fgAll.fibers,mrvNewGraphWin);
 
-% Run the preprocessing
-[dtFile, outBaseDir] = dtiInit(dwRawFileName, t1FileName, dwp);
+%% Build the LiFE model
 
-%% Run MRtrix tractography
-tic, fibersFolder = 'mrtrix_fascicles';
-if ~exist(fibersFolder,'dir'), mkdir(fibersFolder); end
-dt6_file = fullfile(dataDir,dt6_dir_name,'dti96trilin','dt6.mat');
-nFascicles = 120000;
-[status, ~, fg] = feTrack({'prob'}, dt6_file,fibersFolder,mrtrix_findlmax(90),nFascicles);
-toc
-       
-%% Run AFQ
+% Key files
+dwiFile =  fullfile(dataDir,'run01_fliprot_aligned_trilin_aligned_trilin.nii.gz');
+feName = 'afqModel'; 
+feDir  = fullfile(dataDir,'LiFE');
+t1File = fullfile(dataDir,'anatomy','t1.nii.gz');
+feFullFname = fullfile(feDir,[feName,'.mat']);
 
-% Segment the fibers using AFQ
-tic, [fg_classified,~,classification]= AFQ_SegmentFiberGroups(dt6_file, fg);
-toc
-
-%% Split the fiber groups into individual groups
-tic, fascicles = fg2Array(fg_classified);
-toc
-
-% Write the fascicles down to disk as independent files
-tic, afqFolder = fullfile(dataDir,dt6_dir_name, 'AFQ');
-if ~exist(afqFolder,'dir'), mkdir(afqFolder); end
-for iif = 1:length(fascicles)
-    fgWrite(fascicles(iif),    fullfile(afqFolder,[fascicles(iif).name,'_uncleaned']),'mat')
+if exist(feFullFname,'file')
+    load(feFullFname);
+else
+    % Initialize the fascicle evaluation structure
+    fe = feConnectomeInit(dwiFile,fgAll,feName,feDir,dwiFile,t1File);
+    
+    % Do the fit and store the answer in the fe structure
+    fe = feSet(fe,'fit',feFitModel(feGet(fe,'mfiber'),feGet(fe,'dsigdemeaned'),'bbnnls'));
+    
+    % Save the result
+    feConnectomeSave(fe)
 end
 
-% Clean the fibers, we apply the same trhesholds to all fiber
-% groups this is the default thrshold used by AFQ. This is done by
-% not passing opts
-tic, [fascicles1, classification] = feAfqRemoveFascicleOutliers(fascicles,classification);
-toc
+%% Visualize the culled fibers, those with large weights
 
-% Write the fascicles down to disk as independent files
-tic, afqFolder = fullfile(dataDir,dt6_dir_name, 'AFQ');
-if ~exist(afqFolder,'dir'), mkdir(afqFolder); end
-for iif = 1:length(fascicles)
-    fgWrite(fascicles1(iif),    fullfile(afqFolder,fascicles(iif).name),'mat')
+w = feGet(fe,'fiber weights');
+% top20 = prctile(w,80);
+lst = (w > 1e-6);
+fgO = feGet(fe,'fibers acpc');
+fgO = fgExtract(fgO,lst,'keep');
+
+feName = 'afqModelCulled'; 
+feFullFname = fullfile(feDir,[feName,'.mat']);
+
+if exist(feFullFname,'file')
+    load(feFullFname);
+else
+    % Initialize the fascicle evaluation structure
+    fe = feConnectomeInit(dwiFile,fgAll,feName,feDir,dwiFile,t1File);
+    
+    % Do the fit and store the answer in the fe structure
+    fe = feSet(fe,'fit',feFitModel(feGet(fe,'mfiber'),feGet(fe,'dsigdemeaned'),'bbnnls'));
+    
+    % Save the result
+    feConnectomeSave(fe)
 end
 
-% Save the segemented fascicles and the indices into the Mfiber
-classFile2Save = fullfile(afqFolder,'tracts_classification_indices');
-save(classFile2Save,'fg_classified','classification','fascicles1')
+% Have a look at the most important fibers.  
+% mbaDisplayConnectome(fgO.fibers,mrvNewGraphWin);
 
-%% Build the LiFE model.
-dwiFile =  '/home/frk/2t1/fp96_data_variability/dt6_run01_fliprot__4mm/run01_fliprot_aligned_trilin_aligned_trilin.nii.gz';
-feFileName = ['fe_structure_',dt6_dir_name];
-tic, feFolder = fullfile(dataDir,dt6_dir_name, 'LiFE');
-if ~exist(feFolder,'dir'), mkdir(feFolder); end
-fe = feConnectomeInit(dwiFile,fg,feFileName,feFolder,dwiFile,t1FileName);
-fe = feSet(fe,'fit',feFitModel(feGet(fe,'mfiber'),feGet(fe,'dsigdemeaned'),'bbnnls'));
+%%  Now, buid a simulation based on the culled AFQ fibers
 
-% Perform a virtual lesion
+% We will create a NIFTI that is built from the predicted diffusion of
+% these fibers plus some of the mean signal in the original.  Since the
+% fibers put in some mean, we will not put all of the mean back in, but
+% maybe 50 or 80 percent of the mean.
+
+% The nifti should be initialized with the header and size of the original
+% DWI nifti. The anisotropic signal will be only at voxels of the
+% fibers in fg. The anisotropic signal will be the contribution from each
+% fiber node times the fiber weight. 
+%
+% We will then decide how to add in an isotropic term.  One possibility is
+% to add in something proportional to the original NIFTI isotropic
+% component.
+%
+% To do the above we will write a modified version of
+%
+%    feConnectomeBuildModel 
+% 
+% that does not remove the mean of the prediction signal, and that allows
+% adding in an additional isotropic signal.
+%
+% Remember to check warning: 
+% Warning: redundant nodes 
+% > In feComputeVoxelSignal at 31
+%   In feConnectomeBuildModel>(parfor body) at 83
+%   In parallel_function>make_general_channel/channel_general at 870
+%   In remoteParallelFunction at 30 
+
+zeroMean = false;
+fe = feConnectomeBuildModel(fe,zeroMean);
+fe = feSet(fe,'name','ForwardModel');
+nVoxels = feGet(fe,'n voxels');
+v2fn = feGet(fe,'voxel 2 fn pair');
+
+% Xfrom fibers in IMG then find the index in the fe structure
+c = fe.fg.fibers{100};
+plot3(c(1,:),c(2,:),c(3,:),'-o')
+
+% Find the index in the fe structure
+foundVoxels = feGet(fe,'find voxels',c');
+sub2ind(12,31,24)
+
+% These are the voxels in 'whichFiber'
+voxList = [];
+whichFiber = 375;
+for ii=1:nVoxels
+    if any(ismember(v2fn{ii}(:,1),whichFiber))
+        voxList(end+1) = ii;
+    end
+end
+
+voxCoords = feGet(fe,'roi coords');
+voxCoords = voxCoords(voxList,:);
+mrvNewGraphWin;
+plot3(voxCoords(:,1),voxCoords(:,2),voxCoords(:,3),'-o');
+axis on; grid on
+
+
+
+
+
+
+
